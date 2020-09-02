e05163fa8582e37239c0504615cef9b4dccbbfd0
Kevin Daly <dalykb34@gmail.com>
2020-09-02 09:55:04 -0500

    Add project files.

---
+++ BasicClassesProject/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClassesProject
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public string Nickname { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public string Upgrades { get; set; }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine($"{EngineNoise}");
+        }
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
+    }
+}
---
+++ BasicClassesProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BasicClassesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstCar = new Car();
+            firstCar.Make = "Chevy";
+            firstCar.Model = "Colorado";
+            firstCar.Year = 2018;
+
+
+            Console.WriteLine($"My car is a {firstCar.Year} {firstCar.Make} {firstCar.Model}!");
+
+
+            var yourCar = new Car();
+            Console.WriteLine($"What make is your vehicle?");
+            yourCar.Make = Console.ReadLine();
+            Console.WriteLine($"What model is your vehicle?");
+            yourCar.Model = Console.ReadLine();
+            Console.WriteLine($"What year was your vehicle made?");
+            yourCar.Year =  Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine($"Your car is a {yourCar.Year} {yourCar.Make} {yourCar.Model}!");
+        }
+    }
+}
